db3657af3b99850d92339b3f2d27705069e1c8e9
Firoz kumar <56572016+firozbaba@users.noreply.github.com>
2020-03-22 16:41:37 +0530

    Create gcd_and_LCM.cpp

---
+++ gcd_and_LCM.cpp
@@ -0,0 +1,29 @@
+
+Take the following as input.
+
+A number (N1)
+A number (N2)
+Write a function which returns the LCM of N1 and N2. Print the value returned.
+
+Sample Input
+4 
+6
+Sample Output
+12
+Explanation
+The smallest number that is divisible by both N1 and N2 is called the LCM of N1 and N2.
+
+__gcd(a,b) is inbuild function,(b>a)
+
+******lcm*gcd=a*b;always****
+
+#include<iostream>
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+	int a,b;
+	cin>>a>>b;
+	int x=__gcd(a,b);
+	cout<<a*b/x;
+	return 0;
+}
