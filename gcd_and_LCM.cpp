ee52c1f36f1e399593071a7380d24bbde3560060
Firoz kumar <56572016+Firoz-kumar-3@users.noreply.github.com>
2020-04-01 20:14:26 +0530

    Update gcd_and_LCM.cpp

--- gcd_and_LCM.cpp
+++ gcd_and_LCM.cpp
@@ -27,3 +27,28 @@ int main() {
 	cout<<a*b/x;
 	return 0;
 }
+
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+int gcd(int a,int b)
+{
+    if (b==0)
+     return a;
+    else
+     return gcd(b,a%b);
+}
+
+
+int main() {
+	//code
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int a,b;
+	    cin>>a>>b;
+	    cout<<gcd(a,b)<<endl;
+	}
+	return 0;
+}
